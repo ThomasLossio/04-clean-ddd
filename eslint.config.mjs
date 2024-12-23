e1afa9276e494332c58f3e8c64b86eb370c6c590
Thomas Lossio <thomaslossio@hotmail.com>
2024-12-23 19:02:28 -0300

    Delete a question Use Case

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ export default [
       camelcase: 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-empty-object-type': 'on',
       '@stylistic/max-len': ['warn', {
         code: 120,
         tabWidth: 2,
